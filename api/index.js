b61a13faf770afe902bf993f66a5f92f8360d395
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-04-06 15:07:23 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { dbConnection } from './database/dbConnection.js';
+import userRoutes from './routes/user.route.js';
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
+
+app.use('/api/user', userRoutes);
 
 dbConnection();
 app.listen(process.env.PORT, () => {
