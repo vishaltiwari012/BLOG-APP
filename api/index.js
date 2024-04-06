de5534944761fc5d22c93af256a37c6213ee6fbf
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-04-06 15:00:58 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { dbConnection } from './database/dbConnection.js';
 const app = express();
 dotenv.config();
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 dbConnection();
 app.listen(process.env.PORT, () => {
---
+++ api/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import validator from 'validator'
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        username : true
+    },
+    email : {
+        type : String,
+        required : [validator.isEmail, 'Please enter correct email id'],
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+},
+{timestamps : true}
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
+
+    // profilePicture : {
+    //     type:String,
+    //     default:
+    //         'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    // },
+    // isAdmin: {
+    //     type: Boolean,
+    //     default: false,
+    // },
