2956dec0032600bc1b02c9a384ef3eca1ffdbcc4
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-04-13 02:35:17 +0530

    completed update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { signout, test } from '../controllers/user.controller.js';
+import { signout, test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 const router = express.Router();
 
 router.get("/test", test);
-router.post('/signout', signout);
+router.post('/signout', verifyToken, signout);
+router.put('/update/:userId', verifyToken, updateUser);
 
 export default router;
