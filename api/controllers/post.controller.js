c550a1ed263ea2b6332392d6f70126cfbc67e96f
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-04-14 03:09:56 +0530

    complete delete post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -69,3 +69,16 @@ export const getPosts = asyncHandler(async(req, res, next) => {
         next(error);
     }
 });
+
+//Delete post api
+export const deletePost = asyncHandler(async(req, res, next) => {
+    if(!req.user.isAdmin || req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to delete this post'));
+    }
+    try {
+        await Post.findByIdAndDelete(req.params.postId);
+        res.status(200).json("Post deleted successfully!!");
+    } catch(error) {
+        next(error);
+    }
+})
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js';
-import { createPost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.get('/getposts', getPosts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletePost);
 
 export default router;
