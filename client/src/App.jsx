282838a94d7e500f6dcf50c036aef662782d6a4b
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-04-14 01:22:59 +0530

    complete create post ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
             <Route path='/create-post' element={<CreatePost/>}/>
         </Route>
         <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/post/:postSlug' element={<PostPage/>}/>
       </Routes>
       <Footer/>
       <Toaster/>
